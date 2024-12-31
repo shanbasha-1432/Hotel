6f35e324e7df62e5d25a2eac6d23515f8ff70f5b
shanbasha-1432 <shanbashashaik6@gmail.com>
2024-12-31 12:57:40 +0530

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -1,20 +1,20 @@
 const Order=require('../models/orderschema');
 const createOrder=async(req,res)=>{
-    try{
-        const{items:name,price,quantity}=req.body;
-        const order=new Order({items:name,price,quantity})
-
-        await order.save();
+ try{
+         const{orderdetails:name,price,quantity}=req.body;
+         const{user:userid}=req.body;
+         const{orders}=req.body;
+         const order=new Order({orderdetails:name,price,quantity,orders,user:userid})
+       
+        
+        
+         await order.save();
         res.status(201).json({message:'order places successfully',order});
     }
       
             catch (error) {
                 res.status(400).json({ message: 'order not placed.please try again', error})
             }
-   
-            
-
+           
         };
    module.exports=createOrder;
-
-
