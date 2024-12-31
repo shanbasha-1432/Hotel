fde5571428dd054e780f7a42f142eda7cb8af821
shanbasha-1432 <shanbashashaik6@gmail.com>
2024-12-31 16:21:48 +0530

    Update addtocart.js

--- controllers/addtocart.js
+++ controllers/addtocart.js
@@ -1,42 +1,3 @@
-// const CartSchema = require('../models/cartschemamain');
-
-
-// const cartdata = async (req, res) => {
-//     try {
-       
-//         const {userId,items:[{itemId,price,quantity}], } = req.body;
-//         // const { userId, items } = req.body;
-//         cart = new CartSchema({
-
-//             // userId,
-//             //     items: items.map(item => ({
-//             //         itemId: item.itemId,
-//             //         price: item.price,
-//             //         quantity: item.quantity,
-//             //         subtotal: item.price * item.quantity, 
-//             //     })),
-//             //     totalPrice: items.reduce((total, item) => total + item.price * item.quantity, 0),
-//             //     totalQuantity: items.reduce((quantity, item) => quantity + item.quantity, 0),
-
-//             userId,
-//             items:[{itemId,
-//                 price,
-//                 quantity,
-//                 subtotal:price*quantity}],
-//             totalPrice:price*quantity,
-//             totalQuantity:quantity   
-            
-//         });
-       
-//             await cart.save();
-//             res.status(201).json({ message: 'Item added to cart', cart });
-//         } catch (error) {
-//             res.status(500).json({ message: 'Error processing request', error });
-//         }
-//     }
-// module.exports =  cartdata ;
-
-
 const CartSchema = require('../models/cartschemamain');
 
 const cartdata = async (req, res) => {
@@ -60,13 +21,18 @@ const cartdata = async (req, res) => {
 
             } else {
 
-                cart.items.push({ itemId, price, quantity,subtotal });
+                 cart.items.push({ itemId, price, quantity,subtotal });
             }
-
-
+          
+            cart.totalPrice = 0;
+            cart.totalQuantity = 0;
+            cart.items.forEach(item => {
+                cart.totalPrice += item.subtotal;
+                cart.totalQuantity += item.quantity;
+            });
             // cart.totalPrice += subtotal;
-            cart.totalPrice = existingItem.subtotal;
-            cart.totalQuantity = existingItem.quantity;
+            // cart.totalPrice = existingItem.subtotal;
+            // cart.totalQuantity = existingItem.quantity;
             
         } else {
             
@@ -85,7 +51,6 @@ const cartdata = async (req, res) => {
 
         
         await cart.save();
-
         res.status(201).json({ message: 'Item added to cart', cart });
     } catch (error) {
         res.status(500).json({ message: 'Error processing request', error });
