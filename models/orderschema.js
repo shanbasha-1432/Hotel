e672e95b64f81dd4f609c39f5e68786fc1350c04
shanbasha-1432 <shanbashashaik6@gmail.com>
2024-12-31 12:57:58 +0530

    Update orderschema.js

--- models/orderschema.js
+++ models/orderschema.js
@@ -1,22 +1,28 @@
 const mongoose = require('mongoose');
 
-
 const OrderSchema = new mongoose.Schema({
-    items: [{
-        name: {
-            type:String,
-            required: true
+    orders: [
+      {
+        user: {
+          userid: { type: Number,
+             required: true }
         },
-        price:{
-            type: Number,
-            required: true
-        },
-        quantity:{
-            type: Number,
-            required: true
-        }
-    }],
-});
-const Order = mongoose.model('Order', OrderSchema);
-
-module.exports = Order;
+        orderdetails: [
+          {
+            name: { type: String, 
+              required: true
+             },
+            price: { type: Number,
+               required: true 
+              },
+            quantity: { type: Number, 
+              required: true 
+            }
+          }
+        ]
+      }
+    ]
+  });
+  
+  const Order = mongoose.model('Order', OrderSchema);
+  module.exports=Order;
