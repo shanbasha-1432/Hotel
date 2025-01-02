7c1382c36fda81ce58aaced4f2f424a2cc42e354
shanbasha-1432 <shanbashashaik6@gmail.com>
2025-01-02 15:31:15 +0530

    Update removefromcart.js

--- controllers/removefromcart.js
+++ controllers/removefromcart.js
@@ -7,6 +7,7 @@ const removefromcart = async (req, res) => {
                        // const item = await cart.items.itemId.find({itemId,userId})
                        const item = cart.items.find((item) => item.itemId.toString() === itemId);
                        await cart.deleteOne({item})
+         await cart.save();             
         res.status(200).json({ message: 'Item removed from cart',cart });
     } catch (error) {
         res.status(500).json({ message: 'Error processing request', error });
