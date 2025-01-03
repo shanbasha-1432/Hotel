e8e13a89e2774ab1ed69853b736e07935679607c
shanbasha-1432 <shanbashashaik6@gmail.com>
2025-01-03 12:40:37 +0530

    new

--- models/registerschema.js
+++ models/registerschema.js
@@ -1,7 +1,13 @@
 const mongoose =require('mongoose');
+const {v4 : uuidv4} =require('uuid');
 
 const registerSchema =new mongoose.Schema({
-
+    userId:{
+        type:String,
+        default:uuidv4,
+        required:true,
+    },
+    
     name:{
         type:String,
         required:true,
